22b9e76b2b3cf976528658bbda7fc3baa05d64f7
shilpi147 <43372286+shilpi147@users.noreply.github.com>
2018-09-18 16:19:36 +0530

    Update add.c

--- add.c
+++ add.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-   int m, n, c, d, first[10][10], second[10][10], sum[10][10];
+   int m, n, c, d, first[10][10], second[10][10], sub[10][10];
 
    printf("Enter the number of rows and columns of matrix\n");
    scanf("%d%d", &m, &n);
@@ -20,11 +20,11 @@ int main()
       for (d = 0 ; d < n; d++)
          scanf("%d", &second[c][d]);
 
-   printf("Sum of entered matrices:-\n");
+   printf("Sub of entered matrices:-\n");
 
    for (c = 0; c < m; c++) {
       for (d = 0 ; d < n; d++) {
-         sum[c][d] = first[c][d] + second[c][d];
+         sum[c][d] = first[c][d] - second[c][d];
          printf("%d\t", sum[c][d]);
       }
       printf("\n");
